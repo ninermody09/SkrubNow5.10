fd0a2a0a032d9621d8a0550226a4dba8e5dad289
Harsh Mody <harshmody@harshs-macbook>
2018-01-23 15:39:03 -0500

    made a lot of changes

--- SkrubNow/ViewController.swift
+++ SkrubNow/ViewController.swift
@@ -12,111 +12,55 @@ import MapKit
 import Stripe
 
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate {
     
+    
+    @IBOutlet weak var searchBAr: UISearchBar!
     let kBaseURL = "http://lowcost-env.rrpikpmqwu.us-east-1.elasticbeanstalk.com/charge";
     let locationManager = CLLocationManager()
     var hasFoundLocation = false
     var mapIsLoaded = false
     var valueForMap = NSMutableArray()
+    var numberOfResults = Int()
     let firebaseSecureData = FirebaseData.init()
     var successfulTransactionApproved = false
+    var matchingItems:[MKMapItem] = []
     @IBOutlet weak var mapKitView: MKMapView!
     @IBOutlet weak var requestWashButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        firebaseSecureData.loadClientData()
+
+        //searchBar
+        searchBAr.delegate = self
+        //mapkit
         self.mapKitView.delegate = self
         self.mapKitView.showsUserLocation = true
         self.mapKitView.layer.cornerRadius = 10.0
+        self.requestWashButton.layer.cornerRadius = 10.0
         
         locationManager.requestWhenInUseAuthorization()
-        self.requestWashButton.layer.cornerRadius = 10.0
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         firebaseSecureData.loadClientData()
     }
     
-    @IBAction func carInfoButtonPressed(_ sender: AnyObject) {
-        let carInfoVC = CarInformationViewController.init(nibName:"CarInformationViewController", bundle: nil)
-        carInfoVC.view.frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-        self.addChildViewController(carInfoVC)
-        self.view.addSubview(carInfoVC.view)
-    }
-    @IBAction func scheduleButtonPressed(_ sender: AnyObject) {
-        if(FirebaseData.ScheduleData.hasAvailableTimeReturned){
-            
-            let scheduleAppointmentVC = ScheduleViewController.init(nibName:"ScheduleViewController", bundle: nil)
-            scheduleAppointmentVC.view.frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-            self.addChildViewController(scheduleAppointmentVC)
-            self.view.addSubview(scheduleAppointmentVC.view)
-        }
+
+    
+    @IBAction func requestButtonPressed(_ sender: AnyObject) {
+        let requestVC = acuityWebPageViewController.init(nibName:"acuityWebPageViewController", bundle: nil)
+        requestVC.view.frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        self.addChildViewController(requestVC)
+        self.view.addSubview(requestVC.view)
         
     }
-    @IBAction func paymentButtonPressed(_ sender: AnyObject) {
-        let paymentVC = CreditCardViewController.init(nibName:"CreditCardViewController", bundle: nil)
-        paymentVC.view.frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-        self.addChildViewController(paymentVC)
-        self.view.addSubview(paymentVC.view)
-    }
     
-    func postStripeToken(token: STPToken) {
-        var request = URLRequest(url: URL(string: kBaseURL)!)
-        request.httpMethod = "POST"
-        let postString = "amount=500&stripeToken=" + token.tokenId
-        request.httpBody = postString.data(using: .utf8)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard error == nil else {
-                print(error!)
-                return
-            }
-            guard let data = data else {
-                print("Data is empty")
-                return
-            }
-            let json = try! JSONSerialization.jsonObject(with: data, options: [])
-            print(json)
-            self.successfulTransactionApproved = true
-        }
-        task.resume()
-        task.suspend()
-    }
+    //Delegate methods for Mapkid
     
     func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
         self.mapIsLoaded = true
-    }
-    
-    @IBAction func requestButtonPressed(_ sender: AnyObject) {
-        
-        let stripCard = STPCardParams()
-        
-        // Send the card info to Strip to get the token
-        stripCard.number = FirebaseData.CardData.ccNumber
-        stripCard.cvc = FirebaseData.CardData.cvcCode
-        stripCard.expMonth = FirebaseData.CardData.expMonth
-        stripCard.expYear = FirebaseData.CardData.expYear
-        stripCard.name = FirebaseData.CardData.nameOnCard
-        
-        STPAPIClient.shared().createToken(withCard: stripCard, completion: {(token, error) -> Void in
-            
-            if error != nil {
-                self.handleError(error: error! as NSError)
-                NSLog("Unsuccesful Transaction")
-                return
-            }
-            self.postStripeToken(token: token!)
-        })
-        if(self.successfulTransactionApproved){
-            let doneVC = RequestedWashViewController.init(nibName:"RequestedWashViewController", bundle: nil)
-            doneVC.view.frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-            self.addChildViewController(doneVC)
-            self.view.addSubview(doneVC.view)
-            FirebaseData.CardData.ccNumber = ""
-            FirebaseData.CardData.cvcCode = ""
-            self.successfulTransactionApproved = false
-        }
     }
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
@@ -132,6 +76,75 @@ class ViewController: UIViewController, MKMapViewDelegate {
             hasFoundLocation = true
         }
     }
+    
+    //searchbar delegate methods
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBar.text
+        request.region = mapKitView.region
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else {
+                let alertController = UIAlertController(title: nil, message: "Place Not Found", preferredStyle: UIAlertControllerStyle.alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            
+            let pointAnnotation = MKPointAnnotation()
+            pointAnnotation.title = searchBar.text
+            pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: response.boundingRegion.center.latitude, longitude: response.boundingRegion.center.longitude)
+         
+            self.mapKitView.centerCoordinate = pointAnnotation.coordinate
+            
+            self.matchingItems = response.mapItems
+            NSLog("fffS")
+        }
+        searchBar.resignFirstResponder()
+        /*
+        
+        //2
+        let localSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        
+        let localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { (localSearchResponse, error) -> Void in
+            
+            if localSearchResponse == nil{
+                let alertController = UIAlertController(title: nil, message: "Place Not Found", preferredStyle: UIAlertControllerStyle.alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            
+            self.matchingItems = (localSearchResponse?.mapItems)!
+            self.numberOfResults = (localSearchResponse?.mapItems.count)!
+            //3
+            let pointAnnotation = MKPointAnnotation()
+            pointAnnotation.title = searchBar.text
+            pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude:     localSearchResponse!.boundingRegion.center.longitude)
+            
+            
+            //let pinAnnotationView = MKPinAnnotationView(annotation: pointAnnotation, reuseIdentifier: nil)
+            self.mapKitView.centerCoordinate = pointAnnotation.coordinate
+           // self.mapKitView.addAnnotation(pinAnnotationView.annotation!)
+        }
+       //  NSLog("blahh %@", self.matchingItems.count)
+        
+ */
+        
+    }
+    
+    
+    
+    //Error handling
     
     func handleError(error: NSError) {
         UIAlertController(title: "Hmmm there seems to be an error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.alert).show(self, sender: self)
---
+++ SkrubNow/acuityWebPageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  acuityWebPageViewController.swift
+//  SkrubNow
+//
+//  Created by Harsh Mody on 1/22/18.
+//  Copyright © 2018 Harsh. All rights reserved.
+//
+
+import UIKit
+
+class acuityWebPageViewController: UIViewController {
+
+    @IBOutlet weak var webview: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.initScheduler()
+    }
+
+    func initScheduler() {
+        let embeddedHTML = "<iframe src=\"https://app.acuityscheduling.com/schedule.php?owner=15026021\" width=\"100%\" height=\"800\" frameBorder=\"0\"></iframe><script src=\"https://d3gxy7nm8y4yjr.cloudfront.net/js/embed.js\" type=\"text/javascript\"></script>"
+        
+        self.webview.loadHTMLString(embeddedHTML, baseURL: nil)
+        
+    }
+
+
+}
